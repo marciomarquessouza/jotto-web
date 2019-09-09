f750f98fec56f612fb2ecf3880c1198eba4ac271
Marcio Marques de Souza <marcio.souza@dextra-sw.com>
2019-09-08 23:25:49 -0300

    feat: GuessedWords component when no exists guessed words

--- src/components/guessedWords/GuessedWords.js
+++ src/components/guessedWords/GuessedWords.js
@@ -1,8 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const GuessedWords = (props) => (
-    <div></div>
-);
+const GuessedWords = (props) => {
+    if (props.guessedWords.length > 0) {
+        return (
+            <div data-test="component-guessed-words">
+                { props.guessedWords.map((guessedWord) => (
+                    <div data-test="guessed-word" key={guessedWord.word}>
+                        <span>{guessedWord.word}</span>
+                        <span>{guessedWord.match}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    return (<div data-test="no-guessed-word">Try to guess the secret word!</div>);
+};
+
+GuessedWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            word: PropTypes.string.isRequired,
+            match: PropTypes.number.isRequired
+        })
+    ).isRequired,
+}
 
 export default GuessedWords;
--- src/components/guessedWords/GuessedWords.test.js
+++ src/components/guessedWords/GuessedWords.test.js
@@ -7,6 +7,12 @@ const defaultProps = {
     guessedWords: []
 };
 
+const mockProps = { guessedWords: [
+    { word: "river", match: 2 },
+    { word: "sower", match: 1 },
+    { word: "sorry", match: 2 },
+]};
+
 /**
  * Factory function to create ShallowWrapper for the GuessedWords component
  * @function setup
@@ -20,17 +26,38 @@ const setup = (props = {}) => {
 }
 
 describe('/components/GuessedWords', () => {
+    it("shouldn't throw warning with expected props ", () => {
+        checkProps(GuessedWords, mockProps);
+    });
+
     it('should renders GuessedWords component correctly', () => {
+        const wrapper = setup(mockProps);
+        const guessedWords = findByDataTest(wrapper, "component-guessed-words");
+        expect(guessedWords.length).toBe(1);
+    });
+
+    it('should renders a specific message when no exist guessed words', () => {
         const wrapper = setup();
-        const guessedWords = findByDataTest(wrapper, "guessed-words-component");
-        expect(guessedWords.length).toBe(0);
+        const noGuessedWords = findByDataTest(wrapper, "no-guessed-word");
+        expect(noGuessedWords.length).toBe(1);
     });
 
     it('should renders all guessed words received by parent component properly', () => {
-        
+        const wrapper = setup(mockProps);
+        const guessedWordsList = findByDataTest(wrapper, "guessed-word");
+        expect(guessedWordsList.length).toBe(3);
     });
 
-    it('should renders nothing when no exist any guessed word', () => {
-        
+    it('should render each guessed word and matchs quantity properly', () => {
+        const wrapper = setup(mockProps);
+        const guessedWordsList = findByDataTest(wrapper, "guessed-word");
+        guessedWordsList.forEach((node) =>{
+            const word = node.childAt(0).text();
+            const match = parseInt(node.childAt(1).text(), 10);
+            expect(
+                mockProps.guessedWords.filter(
+                    (guessed) => (guessed.word === word && guessed.match === match)
+                ).length).toBe(1);
+        })
     });
 });
